aa0d0f5fc320df9d45c2f48b94d8de5206fa8f34
Ebrahim Mangera <ebrahimmangera9@gmail.com>
2023-03-15 21:04:59 +0000

    commit

---
+++ programming_trails/pointers-1-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - adrees of variables
+ *
+ * Return : Always 0.
+ */
+
+int main(void)
+{
+	char c;
+	char n;
+
+	printf("Address of variable 'c': %p\n", &c);
+	printf("Address of variable 'n': %p\n", &c);
+	return (0);
+}
